217d52ddb7e79679bf1e5c3271c32fd49a1216b9
Kaitlyn Dwelle <kdwelle@mit.edu>
2018-02-20 15:17:26 -0500

    added nearly blank files for electrode boundaries

---
+++ fix_electrodeboundaries.hpp
@@ -0,0 +1,38 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+
+#ifdef FIX_CLASS
+
+FixStyle(electrodeboundaries,FixElectrodeBoundaries)
+
+#else
+
+#ifndef LMP_FIX_GCMC_H
+#define LMP_FIX_GCMC_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixElectrodeBoundaries : public Fix {
+ public:
+  FixElectrodeBoundaries(class LAMMPS *, int, char **);
+  ~FixElectrodeBoundaries();
+  int setmask();
+
+};
+}
+
+#endif
+#endif
